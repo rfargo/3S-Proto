16fab5d2a7245da5ac11f1ef8f63b150493019b8
rfargo <renaldy_fargo@yahoo.com>
2020-07-30 13:09:46 +0800

    Updates

---
+++ precache-manifest.01b5d4d9c0589e601ece38cc090209ac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffeb9ea0c9a66aca86f8094283f39c5e",
+    "url": "/3s-proto/index.html"
+  },
+  {
+    "revision": "9a0a6d4f891961a17d03",
+    "url": "/3s-proto/static/css/2.b45b3de8.chunk.css"
+  },
+  {
+    "revision": "ed7ae9e629f1cd30badc",
+    "url": "/3s-proto/static/css/main.d38234ac.chunk.css"
+  },
+  {
+    "revision": "9a0a6d4f891961a17d03",
+    "url": "/3s-proto/static/js/2.3d8d7285.chunk.js"
+  },
+  {
+    "revision": "df04310ad48f4f5411320a1942994090",
+    "url": "/3s-proto/static/js/2.3d8d7285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7ae9e629f1cd30badc",
+    "url": "/3s-proto/static/js/main.aed6cb31.chunk.js"
+  },
+  {
+    "revision": "a0e2f09f17bc05e932cf",
+    "url": "/3s-proto/static/js/runtime-main.6998821c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/3s-proto/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/3s-proto/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/3s-proto/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/3s-proto/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/3s-proto/static/media/slick.f97e3bbf.svg"
+  }
+]);
